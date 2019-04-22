b4aabef5a0a569d45a9d2a0f6ba6cf404330eaf3
kjgamis <kjgamis@live.ca>
2019-04-22 14:05:01 -0400

    add Knock Knock button

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -15,17 +15,32 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func showAlert() {
+    @IBAction func showHitMeAlert() {
 //        print("Hello!")
         
+//        this is the alert prompt
         let alert = UIAlertController(title: "Hello World!", message: "This is my first app", preferredStyle: .alert)
         
+//        this is the action for when an alert prompt is shown
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let action2 = UIAlertAction(title: "Next", style: .default, handler: nil)
         
 //        tells the alert (controller) to add the button to itself
         alert.addAction(action)
+        alert.addAction(action2)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func showKnockKnockAlert() {
+        let alert = UIAlertController(title: "Knock Knock", message: "Who's there?", preferredStyle: .actionSheet)
+        
+        let action = UIAlertAction(title: "Just ME", style: .destructive, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
     }
  
 
