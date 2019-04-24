6afaf42a8e506894041f3db01e317a53c9c8ff2a
kjgamis <kjgamis@live.ca>
2019-04-23 23:45:23 -0400

    add creator screen

---
+++ BullsEye/CreatorViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreatorViewController.swift
+//  BullsEye
+//
+//  Created by KJG on 2019-04-23.
+//  Copyright © 2019 KJG. All rights reserved.
+//
+
+import UIKit
+
+class CreatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func back() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
